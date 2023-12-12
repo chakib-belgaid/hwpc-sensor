a0ea01459823cbab9622fd2751ad327dbee832fd
chakib belgaid <chakib.belgaid@gmail.com>
2023-12-12 14:31:32 +0100

    fix : detection of the wrong cpu family

--- auto_hwpc_sensor.sh
+++ auto_hwpc_sensor.sh
@@ -2,33 +2,34 @@
 
 # This script is used to automatically generate the config file for hwpc-sensor based on the CPU model.
 get_events() {
-    declare -A cpu_family_codename_map=(
-        ["6"]="Skylake"
-        ["7"]="Whiskey Lake"
-        ["8"]="Coffee Lake"
-        ["42"]="Sandy Bridge"
-        ["10"]="Comet Lake"
-    )
 
     declare -A cpu_model_event_map=(
         ["Skylake"]="CPU_CLK_THREAD_UNHALTED:REF_P, CPU_CLK_THREAD_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
-        [" Whiskey Lake"]="CPU_CLK_THREAD_UNHALTED:REF_P, CPU_CLK_THREAD_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
+        ["Whiskey Lake"]="CPU_CLK_THREAD_UNHALTED:REF_P, CPU_CLK_THREAD_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
         ["Coffe Lake"]="CPU_CLK_THREAD_UNHALTED:REF_P, CPU_CLK_THREAD_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
-        ["Intel Sandy Bridge"]="CPU_CLK_UNHALTED:REF_P, CPU_CLK_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
+        ["Sandy Bridge"]="CPU_CLK_UNHALTED:REF_P, CPU_CLK_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
         ["Comet Lake"]="CPU_CLK_UNHALTED:REF_P, CPU_CLK_UNHALTED:THREAD_P, LLC_MISSES,INSTRUCTIONS_RETIRED"
         ["AMD"]='CYCLES_NOT_IN_HALT, RETIRED_INSTRUCTIONS, RETIRED_UOPS '
     )
-
-    cpu_family=$(cat /proc/cpuinfo  | grep "vendor_id" | awk '{print $3}' | head -n 1)
-    if [ "$cpu_family" == "GenuineIntel" ]; then
-        cpu_family=$(cat /proc/cpuinfo | grep "cpu family" | awk '{print $4}' | head -n 1)
-        cpu_model=${cpu_family_codename_map[$cpu_family]}
-    elif [ "$cpu_family" == "AuthenticAMD" ]; then
+    logs=$(hwpc-sensor)
+    logs=${logs,,}
+    if [[ $logs == *"amd"* ]]; then
         cpu_model="AMD"
+    elif [[ $logs == *"skylake"* ]]; then
+        cpu_model="Skylake"
+    elif [[ $logs == *"whiskey lake"* ]]; then
+        cpu_model="Whiskey Lake"
+    elif [[ $logs == *"coffee lake"* ]]; then
+        cpu_model="Coffee Lake"
+    elif [[ $logs == *"sandy bridge"* ]]; then
+        cpu_model="Sandy Bridge"
+    elif [[ $logs == *"comet lake"* ]]; then
+        cpu_model="Comet Lake"
     else
         echo "Unknown CPU family"
         exit 1
     fi
+
         events=${cpu_model_event_map[$cpu_model]}
         echo $events
 }
