074ad3cce5656fab56e2320327b07fc7f35964b9
chakib belgaid <chakib.belgaid@gmail.com>
2024-06-08 15:25:06 +0200

    fix : add ignore_unsupported_events to the json parameters list

--- src/config_json.c
+++ src/config_json.c
@@ -59,6 +59,22 @@ setup_verbose(struct config *config, json_object *verbose_obj)
 }
 
 static int
+setup_ignore_unsupported_events(struct config *config, json_object *ignore_unsupported_events_obj)
+{
+    int ignore_unsupported_events;
+
+    errno = 0;
+    ignore_unsupported_events = json_object_get_int(ignore_unsupported_events_obj);
+    if (errno != 0 || ignore_unsupported_events < 0) {
+        zsys_error("config: json: ignore_unsupported_events value is invalid (boolean or positive integer expected)");
+        return -1;
+    }
+
+    config->sensor.ignore_unsupported_events = (unsigned int) ignore_unsupported_events;
+    return 0;
+}
+
+static int
 setup_cgroup_basepath(struct config *config, json_object *cgroup_basepath_obj)
 {
     const char *cgroup_basepath = NULL;
@@ -369,6 +385,10 @@ process_json_fields(struct config *config, json_object *root)
     {
         if (!strcasecmp(key, "verbose")) {
             if (setup_verbose(config, value)) {
+                return -1;
+            }
+        } else if (!strcasecmp(key, "ignore_unsupported_events")) {
+            if (setup_ignore_unsupported_events(config, value)) {
                 return -1;
             }
         } else if (!strcasecmp(key, "name") || !strcasecmp(key, "sensor-name")) {
